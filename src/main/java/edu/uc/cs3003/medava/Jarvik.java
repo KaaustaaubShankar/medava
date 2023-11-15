2951d08ac149c3a1c7f8a6248d195d8b9eacb827
KaaustaaubShankar <76492384+KaaustaaubShankar@users.noreply.github.com>
2023-11-15 14:02:58 -0500

    Update Jarvik.java

--- src/main/java/edu/uc/cs3003/medava/Jarvik.java
+++ src/main/java/edu/uc/cs3003/medava/Jarvik.java
@@ -1,1 +1,35 @@
 package edu.uc.cs3003.medava;
+
+public class Jarvik implements Shippable {
+
+    //attributes
+    private String serialNumber;
+    //constructor
+    Jarvik(String deviceSerialNumber) {
+        serialNumber = deviceSerialNumber;
+    }
+
+    //getter
+    public String getMedicineName() {
+        return "Jarvik Artificial Heart";
+    }
+
+    //getter
+    public MedicineSchedule getSchedule() {
+        return MedicineSchedule.Uncontrolled;
+    }
+
+    //getter
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    //method to check temperature range
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (30.0 <= lowTemperature && highTemperature <= 90.0) {
+            return true;
+        }
+        return false;
+    }
+
+    
+}
