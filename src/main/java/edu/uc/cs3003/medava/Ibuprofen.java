b72e70de4395e3121d24a7ed7dd200bcf0c3ad90
KaaustaaubShankar <76492384+KaaustaaubShankar@users.noreply.github.com>
2023-11-15 14:02:37 -0500

    Update Ibuprofen.java

--- src/main/java/edu/uc/cs3003/medava/Ibuprofen.java
+++ src/main/java/edu/uc/cs3003/medava/Ibuprofen.java
@@ -1,1 +1,24 @@
 package edu.uc.cs3003.medava;
+public class Ibuprofen extends Medicine {
+
+    //constructor
+    public Ibuprofen() {
+        super("Ibuprofen");
+    }
+    //getter
+    @Override
+    public MedicineSchedule getSchedule() {
+        return MedicineSchedule.Uncontrolled;
+    }
+
+    //allows us to override the default temperature range
+    @Override
+    public double minimumTemperature() {
+        return 30.0;
+    }
+    //allows us to override the default temperature range
+    @Override
+    public double maximumTemperature() {
+        return 90.0;
+    }
+}
