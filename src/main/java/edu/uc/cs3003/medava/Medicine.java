c1957c86f3a33102d37412f1de13d6eb83fffa37
KaaustaaubShankar <76492384+KaaustaaubShankar@users.noreply.github.com>
2023-11-15 14:03:19 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,35 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
+public abstract class Medicine implements Shippable {
+    private String mMedicineName;
 
+    //constructor
+    public  Medicine(String medicineName) {
+        mMedicineName = medicineName;
+    }
+
+    //getter
+    public String getMedicineName() {
+        return mMedicineName;
+    }
+
+    //method to check temperature range
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+
+    
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }    
+    
+    //abstract method that is implemented by child classes
+    public abstract MedicineSchedule getSchedule();
 }
